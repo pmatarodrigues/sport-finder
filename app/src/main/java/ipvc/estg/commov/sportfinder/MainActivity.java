55bf0c77e7af21acd9757463049126ef1bff9e12
joaocosta <joao.pedro.mc96@gmail.com>
2019-03-25 19:32:29 +0000

    Drawable moved to Drawable xxhdpi

--- app/src/main/java/ipvc/estg/commov/sportfinder/MainActivity.java
+++ app/src/main/java/ipvc/estg/commov/sportfinder/MainActivity.java
@@ -15,14 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonLogin = (Button) findViewById(R.id.button_login);
+        /*buttonLogin = (Button) findViewById(R.id.button_login);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ActivityMainMenu.class);
                 MainActivity.this.startActivity(intent);
             }
-        });
+        });*/
 
     }
 
