ecf3dfd3e052d84d82ad7ff04f1ecd52858b1dab
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2019-03-26 11:42:54 +0000

    Add Create Account Layout

---
+++ app/src/main/java/ipvc/estg/commov/sportfinder/ActivityCreateAccount.java
@@ -0,0 +1,13 @@
+package ipvc.estg.commov.sportfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityCreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
