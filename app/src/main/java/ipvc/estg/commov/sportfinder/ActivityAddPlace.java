b0e22dc0636fcb63d1b1a28fa582707082b86be3
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2019-03-26 18:15:35 +0000

    Add map fragment to ActivityAddPlaceMap

--- app/src/main/java/ipvc/estg/commov/sportfinder/ActivityAddPlace.java
+++ app/src/main/java/ipvc/estg/commov/sportfinder/ActivityAddPlace.java
@@ -1,7 +1,10 @@
 package ipvc.estg.commov.sportfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityAddPlace extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class ActivityAddPlace extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place);
+
+        Button btnContinuar = (Button)findViewById(R.id.button_continuar);
+
+        btnContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityAddPlace.this, ActivityAddPlaceMap.class);
+                ActivityAddPlace.this.startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/ipvc/estg/commov/sportfinder/ActivityAddPlaceMap.java
+++ app/src/main/java/ipvc/estg/commov/sportfinder/ActivityAddPlaceMap.java
@@ -1,13 +1,43 @@
 package ipvc.estg.commov.sportfinder;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ActivityAddPlaceMap extends AppCompatActivity {
+
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+
+public class ActivityAddPlaceMap extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place_map);
+
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 }
