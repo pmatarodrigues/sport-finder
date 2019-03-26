0e4f6ca2b542b5c9908c33aeb840a34a81247f31
PedroDomingosCouto <pecouto@ipvc.pt>
2019-03-26 12:02:52 +0000

    change ActivityCreatAccout

--- app/src/main/java/ipvc/estg/commov/sportfinder/ActivityCreateAccount.java
+++ app/src/main/java/ipvc/estg/commov/sportfinder/ActivityCreateAccount.java
@@ -2,12 +2,35 @@ package ipvc.estg.commov.sportfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ActivityCreateAccount extends AppCompatActivity {
+public class ActivityCreateAccount extends AppCompatActivity{
+    EditText et_email,et_password, et_confirmarPassword;
+    Button btn_criarConta;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        //incialização de variaveis
+        //EditText
+        et_email=(EditText)findViewById(R.id.et_email);
+        et_password=(EditText)findViewById(R.id.et_password);
+        et_confirmarPassword=(EditText)findViewById(R.id.et_confirmarPassword);
+
+        //Botões
+        btn_criarConta=(Button)findViewById(R.id.btn_criarConta);
+
+        btn_criarConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(ActivityCreateAccount.this, "Criar Conta", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
