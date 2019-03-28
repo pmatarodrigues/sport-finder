8a39e0c61a8b7aab7f040e8d166ee84dc1fab495
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2019-03-28 11:59:40 +0000

    Create ActivityParqueDetails with map and location

--- app/src/main/java/ipvc/estg/commov/sportfinder/ActivityParqueDetails.java
+++ app/src/main/java/ipvc/estg/commov/sportfinder/ActivityParqueDetails.java
@@ -1,13 +1,52 @@
 package ipvc.estg.commov.sportfinder;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActivityParqueDetails extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ActivityParqueDetails extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    LatLng latLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parque_details);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        latLng = new LatLng(41.6920494, -8.8346252);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        centerMapOnLocation();
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Nome do Parque");
+        mMap.clear();
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.addMarker(markerOptions);
+
+    }
+
+    public void centerMapOnLocation() {
+
+        //LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.clear();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
     }
 }
