fec5c91df39b0a5953d04701c05a8d6e82994522
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2019-04-02 17:10:12 +0100

    Add ActivityGraphs

--- app/src/main/java/ipvc/estg/commov/sportfinder/ActivityMainMenu.java
+++ app/src/main/java/ipvc/estg/commov/sportfinder/ActivityMainMenu.java
@@ -4,11 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,28 +19,60 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 import ipvc.estg.commov.sportfinder.NetworkChangeReceiver;
 
-public class ActivityMainMenu extends AppCompatActivity {
+public class ActivityMainMenu extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback {
 
     Button buttonAdd;
     Button buttonProfile;
     Button buttonSearch;
 
+    // NEEDED TO CHECK FOR NETWORK
     private BroadcastReceiver mNetworkReceiver;
-
     ClassNoInternet classNoInternet;
+
+    // NEEDED FOR GPS
+    protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest locationRequest;
+    int REQUEST_CHECK_SETTINGS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        // NEEDED TO CHECK FOR NETWORK
         mNetworkReceiver = new NetworkChangeReceiver();
-
         classNoInternet = new ClassNoInternet(mNetworkReceiver);
         registerNetworkBroadcastForNougat();
+
+        // GET GOOGLE API CLIENT
+        // NEEDED FOR GPS
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).build();
+        mGoogleApiClient.connect();
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        // -----------------------
+
 
         buttonAdd = (Button) findViewById(R.id.button_add_main_menu);
         buttonProfile = (Button) findViewById(R.id.button_profile_main_menu);
@@ -47,6 +82,7 @@ public class ActivityMainMenu extends AppCompatActivity {
         setupListeners();
     }
 
+    // NEEDED TO CHECK FOR NETWORK
     public void registerNetworkBroadcastForNougat() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             registerReceiver(mNetworkReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
@@ -64,7 +100,6 @@ public class ActivityMainMenu extends AppCompatActivity {
         }
     }
 
-
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -72,7 +107,7 @@ public class ActivityMainMenu extends AppCompatActivity {
     }
 
 
-
+    // -----------------------
     private void setupListeners() {
         this.buttonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,9 +129,79 @@ public class ActivityMainMenu extends AppCompatActivity {
         buttonProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ActivityMainMenu.this, ActivityParqueDetails.class);
+                Intent intent = new Intent(ActivityMainMenu.this, ActivityGraphs.class);
                 ActivityMainMenu.this.startActivity(intent);
             }
         });
+    }
+
+
+    // CHECK GPS DATA AND ASK TO TURN ON
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        PendingResult result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        mGoogleApiClient,
+                        builder.build()
+                );
+        result.setResultCallback(this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull Result result) {
+        final Status status = result.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                // NO need to show the dialog;
+
+                break;
+
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                //  GPS disabled show the user a dialog to turn it on
+                try {
+                    // Show the dialog by calling startResolutionForResult(), and check the result
+                    // in onActivityResult().
+
+                    status.startResolutionForResult(ActivityMainMenu.this, REQUEST_CHECK_SETTINGS);
+
+                } catch (IntentSender.SendIntentException e) {
+
+                    //failed to show dialog
+                }
+                break;
+
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                // Location settings are unavailable so not possible to show any dialog now
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+
+            if (resultCode == RESULT_OK) {
+
+                Toast.makeText(getApplicationContext(), "GPS ativado!", Toast.LENGTH_LONG).show();
+            } else {
+
+                Toast.makeText(getApplicationContext(), "GPS não está ativo!", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
