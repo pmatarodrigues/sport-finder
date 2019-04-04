8fa3bfd72a28ce801fcb1d30596625b25bff30f8
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2019-04-04 19:52:51 +0100

    Changes to graphView

--- app/src/main/java/ipvc/estg/commov/sportfinder/ActivityGraphs.java
+++ app/src/main/java/ipvc/estg/commov/sportfinder/ActivityGraphs.java
@@ -10,6 +10,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -66,7 +69,16 @@ public class ActivityGraphs extends AppCompatActivity {
                             graph.getViewport().setScalable(true);
                             graph.getViewport().setScalableY(true);
 
+                            graph.getViewport().setYAxisBoundsManual(true);
+                            graph.getViewport().setMinY(0);
+                            graph.getViewport().setMaxY(14);
+
+                            graph.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.NONE );
+
                             graph.addSeries(series);
+                            series.setTitle("Pontos / Semana");
+                            graph.getLegendRenderer().setVisible(true);
+                            graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
 
                         }catch (JSONException ex){
 
